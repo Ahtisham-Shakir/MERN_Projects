b61e4b099ffcf6ba7be52e67ee361d847d7bf232
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-07-21 02:09:06 +0500

    Completed Our Front-end

--- tiktok-clone/client/src/components/Video.js
+++ tiktok-clone/client/src/components/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react'
 import './Video.css'
 import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
     const [playing, setPlaying] = useState(false);
     const videoRef = useRef(null);
 
@@ -23,9 +24,10 @@ function Video() {
                 onClick={handleVideoPress}
                 loop
                 ref={videoRef}
-                src='https://v16-webapp.tiktok.com/1a7a15297492bab60dd6c45abd1a1749/62d86ea6/video/tos/useast2a/tos-useast2a-pve-0037-aiso/ec68ec7c85de469e94093cd816c77dab/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1394&bt=697&btag=80000&cs=0&ds=3&ft=eXd.6HKVMyq8Z74Knwe2NyfQml7Gb&mime_type=video_mp4&qs=0&rc=Mzc4OmllO2g2M2RmNzw5Z0BpMzU4OmQ6ZnU8PDMzZjczM0AyLS9gLV4vNTYxMC0vNWMuYSNhbS5rcjRnZy1gLS1kMWNzcw%3D%3D&l=202207201506550102440820401224F415'></video>
+                src={url}></video>
 
-                <VideoFooter/>
+            <VideoFooter channel={channel} description={description} song={song} />
+            <VideoSidebar likes={likes} messages={messages} shares={shares} />
         </div>
     )
 
--- tiktok-clone/client/src/components/VideoSidebar.js
+++ tiktok-clone/client/src/components/VideoSidebar.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './VideoSidebar.css'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import CommentIcon from '@mui/icons-material/Comment';
+import ShareIcon from '@mui/icons-material/Share';
 
-function VideoSidebar() {
-  return (
-    <div>VideoSidebar</div>
-  )
+function VideoSidebar({likes, messages, shares}) {
+    const [liked, setLiked] = useState(false);
+    return (
+        <div className='videoSidebar'>
+            <div className='videoSidebar__button'>
+                {
+                    liked ? (<FavoriteIcon fontSize='large' onClick={() => setLiked(false)} />) :
+                        (<FavoriteBorderIcon fontSize='large' onClick={() => setLiked(true)} />)
+                }
+                <p>{liked? likes +1: likes}</p>
+            </div>
+            <div className='videoSidebar__button'>
+                <CommentIcon fontSize='large' />
+                <p>{messages}</p>
+            </div>
+            <div className='videoSidebar__button'>
+                <ShareIcon fontSize='large' />
+                <p>{shares}</p>
+            </div>
+        </div>
+    )
 }
 
 export default VideoSidebar
