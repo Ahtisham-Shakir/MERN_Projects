0ca6bf26bf81272746b902af1dbfad9be50cc1eb
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-09-06 12:17:44 +0500

    initial setup and project structure

---
+++ whatsApp-clone/whatsapp-mern/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Chat from "./components/chat/Chat";
+import Sidebar from "./components/sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
