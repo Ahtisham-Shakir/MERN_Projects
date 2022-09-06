1bc494b8be0e28db42cfeff4a29df5a58f350d66
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-09-06 16:22:43 +0500

    mui

--- whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
+++ whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__headerLeft"></div>
+        <div className="sidebar__headerRight"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
