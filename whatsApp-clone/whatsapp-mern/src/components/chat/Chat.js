17c1df41826b0502a3da9b16d0b63115a5e067b0
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-09-07 17:32:28 +0500

    worked on chat component

--- whatsApp-clone/whatsapp-mern/src/components/chat/Chat.js
+++ whatsApp-clone/whatsapp-mern/src/components/chat/Chat.js
@@ -1,8 +1,61 @@
 import React from "react";
 import "./Chat.css";
 
+// MUI imports
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+
 function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      {/* Chat Body */}
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Ahtisham</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__receiver chat__message">
+          <span className="chat__name">Ahtisham</span>
+          This is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      {/* chat footer */}
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="type a message" />
+          <button type="submit">Send</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
+++ whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
@@ -7,6 +7,7 @@ import ChatIcon from "@mui/icons-material/Chat";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 import SidebarChat from "../sidebarChat/SidebarChat";
 
 function Sidebar() {
