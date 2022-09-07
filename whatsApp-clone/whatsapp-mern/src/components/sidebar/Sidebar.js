c0e55441a48ed7c8b6d3e423afc2d367d1dc9767
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-09-07 12:26:00 +0500

    worked on sidebar component

--- whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
+++ whatsApp-clone/whatsapp-mern/src/components/sidebar/Sidebar.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./Sidebar.css";
 
+// MUI imports
+import { IconButton, Avatar } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SidebarChat from "../sidebarChat/SidebarChat";
+
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <div className="sidebar__headerLeft"></div>
-        <div className="sidebar__headerRight"></div>
+        <Avatar src="" />
+
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      {/* sidebar Search */}
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="search or start a new chat" />
+        </div>
+      </div>
+
+      {/* sidebarChat */}
+      <div className="sidebar__chat">
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
